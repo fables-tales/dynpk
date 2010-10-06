132fa1a5f1ea20b946d09a38e476dbd5cadb7108
Robert Spanton <rspanton@zepler.net>
2010-10-06 14:54:44 +0100

    Add a config file parser and example config file.
    Currently unused, but the parser's there.

---
+++ config.py
@@ -0,0 +1,24 @@
+import ConfigParser
+
+class Config:
+    def __init__(self, fname):
+        self.parser = ConfigParser.ConfigParser()
+        self.parser.read( fname )
+
+        self.rpms = self._opt_break( "rpms" )
+        self.files = self._opt_break( "files" )
+        self.exclude_paths = self._opt_break( "exclude_paths" )
+        self.library_dirs = self._opt_break( "library_dirs" )
+
+    def _opt_break(self, name):
+        try:
+            return self._break_up( self.parser.get( "dynpk", name ) )
+        except ConfigParser.NoOptionError:
+            return []
+
+    def _break_up(self, s):
+        l = [x.strip() for x in s.split()]
+        return l
+        
+        
+        
